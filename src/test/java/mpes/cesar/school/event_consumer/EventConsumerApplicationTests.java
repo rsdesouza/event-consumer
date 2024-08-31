3d30e760a5b72ed7f351d1f639a13de9a36523c1
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-08-31 13:51:47 -0300

    feat: primeiro commit

---
+++ src/test/java/mpes/cesar/school/event_consumer/EventConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package mpes.cesar.school.event_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
